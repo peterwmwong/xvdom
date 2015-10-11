19b956d1b1a95dda6e5b079c1dc461ee33874e7d
peterwmwong <peter.wm.wong@gmail.com>
2015-10-11 00:00:29 -0500

    [instance-api] Add missing instance._node update

--- src/render.js
+++ src/render.js
@@ -105,7 +105,7 @@ export function renderInstance(instance){
     return node;
   }
 
-  node       = spec.render(instance);
+  instance._node = node = spec.render(instance);
   node.xvdom = instance;
   return node;
 }
